79cbb3d8f239a8dff15468c22da349b0586fe23b
Shahzaib Shahid <shahzaib.shahid@codezac.com>
2019-12-22 16:07:36 +0500

    Updates

---
+++ precache-manifest.446e1bacf388bd66e2ec1db27b4b636e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08753d55234c85b5858974253904f59a",
+    "url": "/monstor=rolodex/index.html"
+  },
+  {
+    "revision": "b40755d50eec352eede2",
+    "url": "/monstor=rolodex/static/css/main.5d5d1d45.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monstor=rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "b40755d50eec352eede2",
+    "url": "/monstor=rolodex/static/js/main.672b4ee3.chunk.js"
+  },
+  {
+    "revision": "62b8251f9056168040c0",
+    "url": "/monstor=rolodex/static/js/runtime-main.611d2249.js"
+  }
+]);
